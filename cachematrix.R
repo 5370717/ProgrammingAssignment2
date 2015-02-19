45683980c450ea2539c87f5a5d8c02a5d9eb26c9
Paul Demydyuk <5370717@gmail.com>
2015-02-19 23:18:35 +0200

    Added TimeSpend feature

--- cachematrix.R
+++ cachematrix.R
@@ -1,40 +1,36 @@
 ## Programming Assignment 2:
 ## Caching the Inverse of a Matrix
 
-
-
 makeCacheMatrix <- function(x = matrix()) {
   ## This function creates a special "matrix" object that can cache its inverse.
-  inversedMatrix <- NULL
+  inverseMatrix <- NULL
   set <- function(y) {
     x <<- y
-    inversedMatrix <<- NULL
+    inverseMatrix <<- NULL
   }
   get <- function() x 
-  setinv <- function(invM) inversedMatrix <<- invM  
-  getinv <- function() inversedMatrix  
+  setinv <- function(invM) inverseMatrix <<- invM  
+  getinv <- function() inverseMatrix  
   list(set = set, get = get,
        setinv = setinv,
        getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
   # This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
   # If the inverse has already been calculated (and the matrix has not changed), 
   # then the cachesolve should retrieve the inverse from the cache.
-  
-  inversedMatrix <- x$getinv()
-  if(!is.null(inversedMatrix)) {
+  t1 <- Sys.time()
+  inverseMatrix <- x$getinv()
+  if(!is.null(inverseMatrix)) {
     message("getting cached data")
-    
-    return(inversedMatrix)
+    message("Time spent: ", Sys.time() - t1 )
+    return(inverseMatrix)
   }
   mat <- x$get()
   inverseMatrix <- solve(mat)
   x$setinv(inverseMatrix)
-  
+  message("Time spent: ", Sys.time() - t1 )
   inverseMatrix      
 }
